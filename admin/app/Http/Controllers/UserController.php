02767de047f91aef3aa7161d51238661771611ab
muhammed <muhammed.sheriff9999@gmail.com>
2020-07-22 19:02:30 +0200

    f commit

---
+++ admin/app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // Get Current location
+    public function get_Location($lon,$lat)
+    {
+      // Insert Location in database
+      // Return Location
+    }
+
+    //Auth
+    // public function __construct()
+    // {
+    // 	$this->middleware('auth');
+    // }
+
+
+
+
+}
---
+++ admin/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      // clear the users table first
+      User::truncate();
+
+      $faker = \Faker\Factory::create();
+
+      $password = Hash::make('toptal');
+
+
+        // And now let's generate a few dozen users for our app:
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+}
